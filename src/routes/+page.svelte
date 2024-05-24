6cee8a915f041b55956e5241c58786111f9b8798
casperdijkstra <casperdijkstra92@gmail.com>
2024-05-24 17:15:42 +0200

    retrieve posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,59 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Blogging app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit apps
-	</h1>
-
+	<div class="image-container">
+		<img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" alt="Welcome"/>
+		<img src="https://avatars.githubusercontent.com/u/62383372?v=4" alt="Casper" />
+	</div>
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+		I am Casper Dijkstra, a software developer and Azure architect working at Freshminds in the Netherlands.
+		<img src="https://komarev.com/ghpvc/?username=cdijkstra&style=plastic,color=yellowgreen" alt="GitHub Profile Views">
+		<br>
+		
+		<li>👨🏽‍💻 Always exploring new technologies (currently Blazor and Azure Machine Learniing).</li>
+		<li>🎯 My goal is to solve problems just once by creating generalized solutions</li>
+		<li>🔭 Passionate about Complex computations with edge cases</li>
+		<li>🌱 I’m looking to collaborate on innovative projects that connect people of make the world a better and more convenient place.</li>
+		<li>📫 How to reach out to me: <a href="mailto:casper.dijkstra@fresh-minds.nl">casper.dijkstra@fresh-minds.nl</a></li>
 
-	<Counter />
+		<h2> 📈 Github stats: </h2>
+		<img src="https://github-readme-stats.vercel.app/api?username=cdijkstra&show_icons=true&theme=radical" alt="GitHub Stats"/>
+
+		<h2> 🧰 Languages and Tools: </h2>
+		<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=cdijkstra&theme=tokyonight" alt="Top tools"/>
+
+		<p>
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/csharp/csharp.png" alt="CSharp" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png" alt="Blazor" height="40" style="vertical-align:top; margin:4px"> 
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png" alt="Python" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://pluralsight.imgix.net/paths/path-icons/c-plus-plus-93c7ddd5cc.png" alt="C++" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://skorpil.cz/sites/default/files/2022-01/1200px-Bash_Logo_Colored.svg_.png" alt="Bash" height="40" style="vertical-align:top; margin:4px">
+			
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png" alt="Azure" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://hsto.org/getpro/habr/post_images/792/138/92e/79213892e85d57177c0ba9864dd75cc6.png" alt="Docker" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/kubernetes/kubernetes.png" alt="Kubernetes" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://user-images.githubusercontent.com/62383372/171868107-f31270ea-c2c0-4a97-abcb-561b75cee968.png" alt="AKS" height="40" style="vertical-align:top; margin:4px">  
+			<img src="https://www.pulumi.com/logos/brand/twitter-card.png" alt="Pulumi" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png" alt="Linux" height="40" style="vertical-align:top; margin:4px">    
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="VS Code" height="40" style="vertical-align:top; margin:4px">
+		</p>
+	</h2>
 </section>
 
 <style>
-	section {
+	.image-container {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.image-container img {
+		width: 80px; /* Adjust the width as needed */
+    	height: 80px; /* Adjust the height as needed */
 	}
 </style>
--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import './blogs.css';
 
-    // import { onMount } from 'svelte';
-    // import marked from 'marked';
-    // import matter from 'gray-matter';
+    import { onMount } from 'svelte';
+    import { marked } from 'marked';
     import { navigate } from 'svelte-routing';
 
+    $: markdownContent = '';
   
     let posts = [{
         title:  "Mastering JMESPath queries in the Azure CLI",
@@ -14,29 +14,51 @@
         image: "jmespath.png",
         summary: "summary",
         slug: "2021-05-22-Azure-Cli-Querying-Tips.md"
+    },
+    {
+        title:  "Looping over complex objects in Azure Pipelines",
+        date: '2022-07-03 15:45:00 +0100',
+        tags: "DevOps Pipelines",
+        image: "pipelines.png",
+        summary: "summary",
+        slug: "2021-05-22-Azure-Cli-Querying-Tips.md"
     }];
   
     function readMore(slug: string) {
         navigate(`/src/blogs/${slug}`);
     }
 
-    // onMount(async () => {
-    //   const postFiles = import.meta.glob('/src/posts/*.md');
-  
-    // });
+    onMount(async () => {
+        try {
+            const response = await fetch(`/src/routes/blogs/posts/2021-05-22-Azure-Cli-Querying-Tips.md`); // Adjust the path to your Markdown files
+            if (response.ok) {
+                const markdownText = await response.text();
+                const [metadata, content] = markdownText.split('---').slice(1);
+                markdownContent = await marked.parse(content);
+                console.log('hi');
+            } else {
+                console.error('Failed to fetch blog post:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error fetching blog post:', error);
+        }
+    });
+
   </script>
   
   <main>
     <h1>Here you can find my blogs</h1>
     <p>Blog count: {posts.length}</p>
   
+    <!-- <div class="markdown-content">{@html markdownContent}</div> -->
+
     {#if posts.length > 0}
       {#each posts as post}
         <article>
-          <h2>{post.title}</h2>
-          <img src="/src/lib/images/{post.image}" alt={post.title} />
-          <p>{post.summary}</p>
-          <button on:click={() => readMore(post.slug)}>Read more</button>
+            <h2>{post.title}</h2>
+            <img src="/src/lib/images/{post.image}" alt={post.title} />
+            <p>{post.summary}</p>
+            <button on:click={() => readMore(post.slug)}>Read more</button>
         </article>
       {/each}
     {:else}
