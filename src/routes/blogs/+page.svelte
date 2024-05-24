f5823dc3029eb1ad25111de36dffb0b191da2c4f
casperdijkstra <casperdijkstra92@gmail.com>
2024-05-24 13:39:58 +0200

    Blog posts

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,8 +1,46 @@
 <script>
 	import './blogs.css';
-</script>
 
-<main>
+    import { onMount } from 'svelte';
+    import marked from 'marked';
+    import matter from 'gray-matter';
+    import { navigate } from 'svelte-routing';
+
+  
+    let posts = [{
+        title:  "Mastering JMESPath queries in the Azure CLI",
+        date: "2021-05-22 15:45:00 +0100",
+        tags: "DevOps Azure jq jmespath",
+        image: "jmespath.png",
+        summary: "summary",
+        slug: "2021-05-22-Azure-Cli-Querying-Tips.md"
+    }];
+  
+    // function readMore(slug: string) {
+    //     navigate(`/src/blogs/${slug}`);
+    // }
+
+    // onMount(async () => {
+    //   const postFiles = import.meta.glob('/src/posts/*.md');
+  
+    // });
+  </script>
+  
+  <main>
     <h1>Here you can find my blogs</h1>
-    <p>This is a new page in your Svelte app.</p>
-</main>
+    <p>Blog count: {posts.length}</p>
+  
+    {#if posts.length > 0}
+      {#each posts as post}
+        <article>
+          <h2>{post.title}</h2>
+          <img src="/src/lib/images/{post.image}" alt={post.title} />
+          <p>{post.summary}</p>
+          <!-- <button on:click={() => readMore(post.slug)}>Read more</button> -->
+        </article>
+      {/each}
+    {:else}
+      <p>Loading...</p>
+    {/if}
+  </main>
+  
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
