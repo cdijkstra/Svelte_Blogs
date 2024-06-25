9227eec8f1bb8cfba3cdc803f826803e96ec350c
Casper <c.d.dijkstra@hva.nl>
2024-06-25 21:13:34 +0200

    Update symbols

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,16 +29,20 @@
 			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/csharp/csharp.png" alt="CSharp" height="40" style="vertical-align:top; margin:4px">
 			<img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png" alt="Blazor" height="40" style="vertical-align:top; margin:4px"> 
 			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png" alt="Python" height="40" style="vertical-align:top; margin:4px">
-			<img src="https://pluralsight.imgix.net/paths/path-icons/c-plus-plus-93c7ddd5cc.png" alt="C++" height="40" style="vertical-align:top; margin:4px">
-			<img src="https://skorpil.cz/sites/default/files/2022-01/1200px-Bash_Logo_Colored.svg_.png" alt="Bash" height="40" style="vertical-align:top; margin:4px">
-			
+			<img src="https://banner2.cleanpng.com/20180408/pew/kisspng-the-c-programming-language-computer-icons-comput-programming-5acadc2dec0be9.0824244915232440779669.jpg" alt="C++" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://media.licdn.com/dms/image/D5612AQH4oLPhUfgZiA/article-cover_image-shrink_720_1280/0/1667313566845?e=2147483647&v=beta&t=-INWC09Mn-fQz8cYHPlAD7l2gtMQUUg-QHpcGkFcLw0" alt="Python" height="40" style="vertical-align:top; margin:4px">
+		</p>
+		<p>
 			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png" alt="Azure" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="VS Code" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfcZoyxK15HjtCBnCrGJm2HqzLFucbvQghVQ&s" alt="Rider" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png" alt="Linux" height="40" style="vertical-align:top; margin:4px">    
+			<img src="https://skorpil.cz/sites/default/files/2022-01/1200px-Bash_Logo_Colored.svg_.png" alt="Bash" height="40" style="vertical-align:top; margin:4px">
+		</p>
+		<p>
 			<img src="https://hsto.org/getpro/habr/post_images/792/138/92e/79213892e85d57177c0ba9864dd75cc6.png" alt="Docker" height="40" style="vertical-align:top; margin:4px">
 			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/kubernetes/kubernetes.png" alt="Kubernetes" height="40" style="vertical-align:top; margin:4px">
-			<img src="https://user-images.githubusercontent.com/62383372/171868107-f31270ea-c2c0-4a97-abcb-561b75cee968.png" alt="AKS" height="40" style="vertical-align:top; margin:4px">  
-			<img src="https://www.pulumi.com/logos/brand/twitter-card.png" alt="Pulumi" height="40" style="vertical-align:top; margin:4px">
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png" alt="Linux" height="40" style="vertical-align:top; margin:4px">    
-			<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="VS Code" height="40" style="vertical-align:top; margin:4px">
+			<img src="https://www.pulumi.com/logos/brand/twitter-summary.png" alt="Pulumi" height="40" style="vertical-align:top; margin:4px">
 		</p>
 	</h2>
 </section>
