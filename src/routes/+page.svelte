a2aaaa8f75a5d692fb62aa9e130fa2f84908deb5
Casper <c.d.dijkstra@hva.nl>
2025-06-26 18:45:09 +0200

    Update profile description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,153 @@
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Blogging app" />
+  <title>Home</title>
+  <meta name="description" content="Blogging app" />
 </svelte:head>
 
 <section>
-	<div class="image-container">
-		<img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" alt="Welcome"/>
-		<img src="/casper2.png" alt="Casper" />
-	</div>
-	<h2>
-		I am Casper Dijkstra, a software developer and Azure architect working at Freshminds in the Netherlands.
-		<img src="https://komarev.com/ghpvc/?username=cdijkstra&style=plastic,color=yellowgreen" alt="GitHub Profile Views">
-		<br>
-		
-		<li>👨🏽‍💻 Always exploring new technologies (currently Blazor and Azure Machine Learning).</li>
-		<li>🎯 My goal is to solve problems just once by creating generalized solutions</li>
-		<li>🔭 Passionate about Complex computations with edge cases</li>
-		<li>🌱 I’m looking to collaborate on innovative projects that connect people of make the world a better and more convenient place.</li>
-		<li>📫 How to reach out to me: <a href="mailto:casper.dijkstra@fresh-minds.nl">casper.dijkstra@fresh-minds.nl</a></li>
+  <div class="image-container">
+    <img
+      src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif"
+      alt="Welcome"
+    />
+    <img src="/casper2.png" alt="Casper" />
+  </div>
+  <h2>
+    I am Casper Dijkstra, a software developer and Azure architect working in
+    the Netherlands.
+    <img
+      src="https://komarev.com/ghpvc/?username=cdijkstra&style=plastic,color=yellowgreen"
+      alt="GitHub Profile Views"
+    />
+    <br />
 
-		<h2> 📈 Github stats: </h2>
-		<img src="https://github-readme-stats.vercel.app/api?username=cdijkstra&show_icons=true&theme=radical" alt="GitHub Stats"/>
+    <li>
+      👨🏽‍💻 Always exploring new technologies (currently Blazor and Azure Machine
+      Learning).
+    </li>
+    <li>
+      🎯 My goal is to solve problems just once by creating generalized
+      solutions
+    </li>
+    <li>🔭 Passionate about Complex computations with edge cases</li>
+    <li>
+      🌱 I’m looking to collaborate on innovative projects that connect people
+      of make the world a better and more convenient place.
+    </li>
+    <li>
+      📫 How to reach out to me: <a href="mailto:casperdijkstra92@gmail.com"
+        >casperdijkstra92@gmail.com</a
+      >
+    </li>
 
-		<h2> 🧰 Languages and Tools: </h2>
-		<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=cdijkstra&theme=tokyonight" alt="Top tools"/>
+    <h2>📈 Github stats:</h2>
+    <img
+      src="https://github-readme-stats.vercel.app/api?username=cdijkstra&show_icons=true&theme=radical"
+      alt="GitHub Stats"
+    />
 
-		<div class="flex-container">
-			<div class="flex-row">
-				<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/csharp/csharp.png" alt="CSharp" height="40">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png" alt="Blazor" height="40">
-				<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png" alt="Python" height="40">
-				<img src="https://banner2.cleanpng.com/20180408/pew/kisspng-the-c-programming-language-computer-icons-comput-programming-5acadc2dec0be9.0824244915232440779669.jpg" alt="C++" height="40">
-				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9A_S6BPxCDRp5WjMFEfXrpCu1ya2OO-Lw&s" alt="Python" height="40">
-				<img src="https://media.licdn.com/dms/image/D5612AQH4oLPhUfgZiA/article-cover_image-shrink_720_1280/0/1667313566845?e=2147483647&v=beta&t=-INWC09Mn-fQz8cYHPlAD7l2gtMQUUg-QHpcGkFcLw0" alt="HTML 5" height="40">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" alt="CSS 3" height="40">
-			</div>
-			<div class="flex-row">
-				<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png" alt="Azure" height="40">
-				<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" alt="VS Code" height="40">
-				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfcZoyxK15HjtCBnCrGJm2HqzLFucbvQghVQ&s" alt="Rider" height="40">
-				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png" alt="Linux" height="40">
-				<img src="https://skorpil.cz/sites/default/files/2022-01/1200px-Bash_Logo_Colored.svg_.png" alt="Bash" height="40">
-			</div>
-			<div class="flex-row">
-				<img src="https://hsto.org/getpro/habr/post_images/792/138/92e/79213892e85d57177c0ba9864dd75cc6.png" alt="Docker" height="40">
-				<img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/kubernetes/kubernetes.png" alt="Kubernetes" height="40">
-				<img src="https://www.pulumi.com/logos/brand/twitter-summary.png" alt="Pulumi" height="40">
-			</div>
-		</div>
-	</h2>
+    <h2>🧰 Languages and Tools:</h2>
+    <img
+      src="https://github-readme-stats.vercel.app/api/top-langs/?username=cdijkstra&theme=tokyonight"
+      alt="Top tools"
+    />
+
+    <div class="flex-container">
+      <div class="flex-row">
+        <img
+          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/csharp/csharp.png"
+          alt="CSharp"
+          height="40"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png"
+          alt="Blazor"
+          height="40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"
+          alt="Python"
+          height="40"
+        />
+        <img
+          src="https://banner2.cleanpng.com/20180408/pew/kisspng-the-c-programming-language-computer-icons-comput-programming-5acadc2dec0be9.0824244915232440779669.jpg"
+          alt="C++"
+          height="40"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9A_S6BPxCDRp5WjMFEfXrpCu1ya2OO-Lw&s"
+          alt="Python"
+          height="40"
+        />
+        <img
+          src="https://media.licdn.com/dms/image/D5612AQH4oLPhUfgZiA/article-cover_image-shrink_720_1280/0/1667313566845?e=2147483647&v=beta&t=-INWC09Mn-fQz8cYHPlAD7l2gtMQUUg-QHpcGkFcLw0"
+          alt="HTML 5"
+          height="40"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+          alt="CSS 3"
+          height="40"
+        />
+      </div>
+      <div class="flex-row">
+        <img
+          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/azure/azure.png"
+          alt="Azure"
+          height="40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png"
+          alt="VS Code"
+          height="40"
+        />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfcZoyxK15HjtCBnCrGJm2HqzLFucbvQghVQ&s"
+          alt="Rider"
+          height="40"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png"
+          alt="Linux"
+          height="40"
+        />
+        <img
+          src="https://skorpil.cz/sites/default/files/2022-01/1200px-Bash_Logo_Colored.svg_.png"
+          alt="Bash"
+          height="40"
+        />
+      </div>
+      <div class="flex-row">
+        <img
+          src="https://hsto.org/getpro/habr/post_images/792/138/92e/79213892e85d57177c0ba9864dd75cc6.png"
+          alt="Docker"
+          height="40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/kubernetes/kubernetes.png"
+          alt="Kubernetes"
+          height="40"
+        />
+        <img
+          src="https://www.pulumi.com/logos/brand/twitter-summary.png"
+          alt="Pulumi"
+          height="40"
+        />
+      </div>
+    </div>
+  </h2>
 </section>
 
 <style>
-	.image-container {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  .image-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
 
-	.image-container img {
-		width: 80px; /* Adjust the width as needed */
-    	height: 80px; /* Adjust the height as needed */
-	}
+  .image-container img {
+    width: 80px; /* Adjust the width as needed */
+    height: 80px; /* Adjust the height as needed */
+  }
 </style>
